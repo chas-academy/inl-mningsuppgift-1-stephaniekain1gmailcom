f3978aad19770472a80700cbed5c52fffc742865
stephaniekain1@gmail.com <stephaniekain1@gmail.com>
2026-03-05 22:14:14 +0100

    Klar

--- src/script.js
+++ src/script.js
@@ -26,7 +26,7 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
+  let groupToShow = "vänner";
   
 
 
@@ -36,8 +36,13 @@ function showContacts(contactBook, groupToShow) { // Rör ej denna kod
 
 
     // Loopar igenom alla kontakter och jämför grupp
-   
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
+
+    for (contact of contactBook.contacts) {
+      if (contact.group === groupToShow) {
+        return `Namn: ${contact.name}, Telefon: ${contact.phone}`; // här ska sträng med namn och tekefonnummer visas i form av string interpolation
+      }
+    }
+  
 
 
 }
